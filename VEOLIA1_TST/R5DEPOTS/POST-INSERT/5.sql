3df463a51c20fd4b297a685b7102247afc646c42
vams-git <109448159+vams-git@users.noreply.github.com>
2024-10-21 16:03:11 +1030

    init R5DEPOTS-POST-INSERT-5

---
+++ VEOLIA1_TST/R5DEPOTS/POST-INSERT/5.sql
@@ -0,0 +1,22 @@
+declare 
+ dep        r5depots%rowtype;
+ 
+ vErrMsg    varchar2(400);
+ err_val    exception;
+
+begin
+ select * into dep from r5depots where rowid=:rowid;
+ if dep.dep_class in ('GIF','CRD') and nvl(dep.dep_external,'-') = '-' then
+    update r5depots
+    set dep_external = '+'
+    where rowid = :rowid
+    and dep_class in ('GIF','CRD') and nvl(dep_external,'-') = '-';
+ end if;
+ 
+ 
+exception
+when err_val then
+   RAISE_APPLICATION_ERROR (-20003,vErrMsg) ;   
+when others then
+   RAISE_APPLICATION_ERROR (-20003,'Error in Flex r5depots/Post Insert/5/'||substr(SQLERRM, 1, 500)) ;   
+end;
