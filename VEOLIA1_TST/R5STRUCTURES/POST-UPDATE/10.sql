6aa7ac9ba96c0d90200ad19a580a2cf80097aeab
vams-git <109448159+vams-git@users.noreply.github.com>
2024-10-21 15:58:04 +1030

    init R5STRUCTURES-POST-UPDATE-10

---
+++ VEOLIA1_TST/R5STRUCTURES/POST-UPDATE/10.sql
@@ -0,0 +1,28 @@
+declare
+   stc           r5structures%rowtype;
+   sorg         number;
+
+begin
+    select * into stc from r5structures where rowid=:rowid;
+
+    select count(1) into sorg from r5organization
+    where org_code = stc.stc_child_org
+       and org_udfchar10 like '%Fleet%';
+
+    if stc.stc_childtype in ('02ET', '03UT','04AS', '05FP') 
+       and nvl(stc.stc_rolldown,'-') = '-' and sorg = 0 then
+       update r5structures
+       set stc_rolldown = '+'
+       where rowid =:rowid;
+    end if;
+
+    if stc.stc_childtype in ('06EQ','07CP' ) and nvl(stc.stc_rolldown,'-') = '-' then
+       update r5structures
+       set stc_rolldown = '+'
+       where rowid =:rowid;
+    end if;
+   
+exception 
+  when others then 
+     RAISE_APPLICATION_ERROR ( -20003, 'Processing error in Flex/r5structures/Update/10') ; 
+end;
