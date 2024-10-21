1b88c3114056688b12a43c07149b51ee516f32b2
vams-git <109448159+vams-git@users.noreply.github.com>
2024-10-21 16:03:41 +1030

    init R5MAINTENANCEPATTERNS-POST-INSERT-5

---
+++ VEOLIA1_TST/R5MAINTENANCEPATTERNS/POST-INSERT/5.sql
@@ -0,0 +1,24 @@
+DECLARE
+  mtp   R5MAINTENANCEPATTERNS%ROWTYPE;
+  err   EXCEPTION;
+  imsg  VARCHAR2(400);
+
+BEGIN
+  SELECT * INTO mtp FROM R5MAINTENANCEPATTERNS
+  WHERE ROWID = :rowid;
+  
+  IF nvl(mtp.mtp_udfchkbox04,'-') != '+' THEN
+    UPDATE R5MAINTENANCEPATTERNS
+    SET   mtp_udfchkbox04 = '+'
+    WHERE mtp_org = mtp.mtp_org
+      AND mtp_code = mtp.mtp_code
+      AND mtp_revision = mtp.mtp_revision;
+  END IF;
+
+EXCEPTION
+  WHEN err THEN
+  RAISE_APPLICATION_ERROR (-20003, imsg);
+  WHEN OTHERS THEN
+  RAISE_APPLICATION_ERROR (-20003,
+    'ERR/R5MAINTENANCEPATTERNS/5/I/'||Substr(SQLERRM, 1, 500));
+END;
