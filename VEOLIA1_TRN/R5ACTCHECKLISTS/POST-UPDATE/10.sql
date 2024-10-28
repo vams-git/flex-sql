78bef2ab883565e539216e1b3b5753ec24bc42a1
vams-git <109448159+vams-git@users.noreply.github.com>
2024-10-28 12:13:08 +1100

    init R5ACTCHECKLISTS-POST-UPDATE-10

---
+++ VEOLIA1_TRN/R5ACTCHECKLISTS/POST-UPDATE/10.sql
@@ -0,0 +1,34 @@
+declare 
+  ack             r5actchecklists%rowtype; 
+  vOrg            r5organization.org_code%type;
+  cOrg            number;
+  vErr            exception;
+begin
+  select * into ack from r5actchecklists where rowid=:rowid;
+  if ack.ack_event is null then
+    return;
+  end if;
+  
+  begin
+      select evt_org into vOrg from r5events where evt_code = ack.ack_event;
+  exception when no_data_found then
+    return;
+  end;
+
+  select count(1) into cOrg from r5organization where org_code = vOrg and org_udfchar10 like '%Fleet%';
+  
+  if cOrg = 0 then
+      if ack.ack_type ='01' and ack.ack_requiredtoclose ='YES' and ack.ack_notes is null and ack.ack_updatedby = o7sess.cur_user then
+          raise vErr;
+      end if;
+      if ack.ack_type ='03' and upper(ack.ack_finding) = 'OTHE'  and ack.ack_notes is null and ack.ack_updatedby = o7sess.cur_user then
+         raise vErr;
+      end if;
+   end if; 
+
+EXCEPTION
+   WHEN vErr then
+   RAISE_APPLICATION_ERROR ( -20003,'Please input a note comment for this checklist item') ;
+   when others then
+   RAISE_APPLICATION_ERROR (  -20003,'Error in Flex R5ACTCHECKLISTS/Post Update/10/'||substr(SQLERRM, 1, 500)) ; 
+end;
