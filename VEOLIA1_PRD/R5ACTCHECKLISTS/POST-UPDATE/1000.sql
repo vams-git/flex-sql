818f5effe07ae06f761f2dbbf0e3bbcf38d70347
vams-git <109448159+vams-git@users.noreply.github.com>
2024-12-07 00:33:27 +1030

    refresh R5ACTCHECKLISTS-POST-UPDATE-1000

--- VEOLIA1_PRD/R5ACTCHECKLISTS/POST-UPDATE/1000.sql
+++ VEOLIA1_PRD/R5ACTCHECKLISTS/POST-UPDATE/1000.sql
@@ -3,6 +3,6 @@ r5events,r5activities,r5actchecklists ack,r5tasks
 where  evt_code = act_event 
 and    act_event = ack_event and act_act = ack_act
 and    act_task = tsk_code and act_taskrev = tsk_revision and tsk_class = 'CORR'
-and    evt_org in ('WYU','HWC','KUR','BAL','TAS','VIC','WAU','WAR','NWA','NSW','SAU','QLD','NTE','NVE','NVP','NVW')
+and    evt_org in ('WYU','HWC','KUR','BAL','TAS','VIC','WAU','WAR','NWA','NSW','SAU','QLD','NTE','NVE','NVP','NVW','NVQ','FCG')
 and    ack_yes = '+' and ack_sequence = 50 and (ack_notes is null or ack_notes='')
 and    ack.rowid=:rowid;
